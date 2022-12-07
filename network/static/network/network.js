a481da594fddd6425c92b416526572e56b6bc764
marciogugo <marcio.gugo@gmail.com>
2022-12-07 20:56:34 -0300

    Reply Button
    Reply button

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#button-like').addEventListener('click', () => like_post());
     document.querySelector('#button-unlike').addEventListener('click', () => unlike_post());
     document.querySelector('#button-edit').addEventListener('click', () => edit_post());
-    document.querySelector('#button-reply').addEventListener('click', () => reply_post());
+//    document.querySelector('#button-reply').addEventListener('click', () => reply_post());
+    document.querySelectorAll('div.post-bt-reply').addEventListener('click', () => reply_post());
 
     document.onreadystatechange = () => {
         if (document.readyState === 'complete') {
@@ -15,6 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
     viewPosts();
 });
 
+
 function loaded() {
 }
 
@@ -24,26 +26,15 @@ function edit_post(id) {
 }
 
 
-function reply_post(id) {
-    const el = document.getElementById('reply_post_view');
+function reply_post() {
+    const id = document.activeElement.getAttribute('name').replace(/\D/g,''); //only numbers
 
-    if (el != null) {
-        alert("entrou")
-        const nome = el.getAttribute('name');
-        alert("pegou")
-
-        if (nome == null) {
-            alert('nome é nulo')
-        } else {
-            alert('reply post ' + nome)
-        }
-
+    if (id != null) {
+        alert(id);
         document.querySelector('#compose-post-view').style.display = 'none';
         document.querySelector('#all-posts-view').style.display='block';
         document.querySelector('#edit-post-view').style.display='none';
-        document.querySelector('#reply-post-view'+id).style.display='block';
-    } else {
-        alert('view é nulo')
+        document.getElementById('post-bt-reply-'||id).style.display='block';
     }
   }
 
